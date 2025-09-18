4235cc96d599e226b71c24e9c145efd3f4f9238f
24DP3ACern <A240672AC@rvt.lv>
2025-09-18 14:33:00 +0300

    yes

--- src/main/java/rvt/Avarage.java
+++ src/main/java/rvt/Avarage.java
@@ -1,7 +1,12 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class Avarage {
     public static void main(String[] args) {
-        
+        Scanner scaner = new Scanner(System.in);
+        int a = 5;
+        int b = 23; 
+        System.out.println( b / Double.valueOf(a));
     }
 }
