9dbda1000fecf25694de9d883796ab96e9eb3d1c
24DP3ACern <A240672AC@rvt.lv>
2025-09-18 14:36:09 +0300

    fge

--- src/main/java/rvt/Avarage.java
+++ src/main/java/rvt/Avarage.java
@@ -6,7 +6,7 @@ public class Avarage {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int a = 5;
-        int b = 23; 
-        System.out.println( b / Double.valueOf(a));
+        int b = 25; 
+        System.out.println(b / Double.valueOf(a));
     }
 }
