530a06bd681dd29391b4bcbbf0573acf37ac1b99
24DP3ACern <A240672AC@rvt.lv>
2025-10-09 14:10:04 +0300

    superappds

--- src/main/java/rvt/Avarage.java
+++ src/main/java/rvt/Avarage.java
@@ -4,27 +4,30 @@ import java.util.Scanner;
 
 public class Avarage {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-       int priceAp = 10;
-       int priceOr = 15;
-       int pricePot = 5;
-       System.out.println("enter apple count (10 per apple): ");
-       int countAp = Integer.valueOf(scanner.nextLine());
-       System.out.println("enter orange count (15 per orange): ");
-       int countOr = Integer.valueOf(scanner.nextLine());
-       System.out.println("enter potater count (10 per potat): ");
-       int countPot = Integer.valueOf(scanner.nextLine());
-       if (countOr != countAp){
-        System.out.println("check your bag, too little oranges");
-       }
-       else if(countAp != (2*countPot)) {
-
-        System.out.println("check your bag, too little potaters");
-       }
-       else {
-        System.out.println("just enough");
-       };
-       System.out.println("that will be: " + (priceAp*countAp+priceOr*countOr+pricePot*countPot));
+        Scanner scanner = new Scanner(System.in);    
+        System.out.println("Give me your numbers ");
+        int sums = 0;
+        int quat = 0;
+        int notodd = 0; 
+        int odd = 0; 
+        while(true){
+            int num = Integer.valueOf(scanner.nextLine());
+            if (num == -1) {
+                System.out.println("ok byee");
+                break;
+            } else if(num % 2 == 0) {
+                notodd++;
+            } else {
+                odd++;
+            } 
+            sums = sums + num;
+            quat++;
+        }
+        System.out.println("yo sums are " + sums);
+        System.out.println(quat + " times");
+        System.out.println("yo average is " + (sums / (double) quat));
+        System.out.println("i forgot what not odds are called but there are " + notodd + " of em");
+        System.out.println("and odds here are " + odd);
     }
     }
 
