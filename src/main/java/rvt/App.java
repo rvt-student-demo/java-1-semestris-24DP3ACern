6d312da36804a98847a9cac9151f7867f76fb97a
24DP3ACern <A240672AC@rvt.lv>
2025-10-09 13:58:29 +0300

    efe2323

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,8 +1,18 @@
 package rvt;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("World");
+    Scanner scanner = new Scanner(System.in);
+    int summ = 0;
+    System.out.println("enter number: ");
+    int num1 = Integer.valueOf(scanner.nextLine());
+    System.out.println("imput another number: ");
+    int numm2 = Integer.valueOf(scanner.nextLine());
+    for (int i = num1; i <= numm2; i++) {
+        summ = summ + i;
     }
-}
+    System.out.println("sum is " + summ);
+        }
+    }   
