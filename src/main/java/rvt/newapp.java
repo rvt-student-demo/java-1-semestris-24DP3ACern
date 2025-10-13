6ff8b27aa40ff96e5bad6af0a355c12318fcca30
24DP3ACern <A240672AC@rvt.lv>
2025-10-13 13:25:29 +0300

    oijij

---
+++ src/main/java/rvt/newapp.java
@@ -0,0 +1,11 @@
+package rvt;
+
+public class newapp {
+    public static void main(String[] args) {
+        System.out.println("wewefwefwefwef");
+            newMeth();
+    }
+    public static void newMeth() {
+        System.out.println("new method");
+    }
+}
