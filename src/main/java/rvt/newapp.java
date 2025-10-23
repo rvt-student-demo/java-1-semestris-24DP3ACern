55276c4cfec2f8e0dad7ba95b7975eda1c1548ca
24DP3ACern <A240672AC@rvt.lv>
2025-10-23 12:27:16 +0300

    arrayssss

--- src/main/java/rvt/newapp.java
+++ src/main/java/rvt/newapp.java
@@ -1,11 +1,34 @@
 package rvt;
-
+import java.util.Scanner;
 public class newapp {
     public static void main(String[] args) {
-        System.out.println("wewefwefwefwef");
-            newMeth();
+       Scanner scanner = new Scanner(System.in);
+    int[] data = new int[10];
+    System.out.println("Set data: ");
+    for (int i=0; i < data.length; i++) {
+        data[i] = scanner.nextInt();
     }
-    public static void newMeth() {
-        System.out.println("new method");
+    System.out.println("so far:");
+    for (int i=0; i < data.length; i++){
+        System.out.println((i+1)+": "+data[i]);
+    }
+    String[] data2 = new String[10];
+    System.out.println("Set data: ");
+    for (int i=0; i < data.length; i++) {
+        data[i] = scanner.nextInt();
+    }
+    System.out.println("so far:");
+    for (int i=0; i < data.length; i++){
+        System.out.println((i+1)+": "+data[i]);
+    }
+    double[] data3 = new double[10];
+    System.out.println("Set data: ");
+    for (int i=0; i < data.length; i++) {
+        data[i] = scanner.nextInt();
+    }
+    System.out.println("so far:");
+    for (int i=0; i < data.length; i++){
+        System.out.println((i+1)+": "+data[i]);
+    }
     }
 }
